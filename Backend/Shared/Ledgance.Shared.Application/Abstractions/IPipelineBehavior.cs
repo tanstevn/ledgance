e8035a3afb0875522edc81732abcd5fd9763aeac
Steven Tan <tanstevenlester@gmail.com>
2026-08-10 16:43:53 +0800

    Delete Backend directory

--- Backend/Shared/Ledgance.Shared.Application/Abstractions/IPipelineBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ledgance.Shared.Application.Abstractions {
-    public interface IPipelineBehavior<in TRequest, TResponse>
-        where TRequest : notnull {
-        Task<TResponse> HandleAsync(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct);
-    }
-
-    public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
-}
